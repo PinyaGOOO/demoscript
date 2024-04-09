a7dbf8995f4e38c1e9a8a3116f2d93168c761ae1
PinyaGOOO <165675370+PinyaGOOO@users.noreply.github.com>
2024-04-09 14:59:51 +0300

    Add files via upload

--- dhcp.sh
+++ dhcp.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 dnf install -y dhcp-server
+echo -n "Napishi MAC path:HQ-SRV=HARDWARE-net0: "
+read mac
 
-echo -e "subnet 172.16.100.0 netmask 255.255.255.192 {\n  range 172.16.100.2 172.16.100.62;\n  option routers 172.16.100.1;\n  default-lease-time 600;\n max-lease-time 7200;\n}\host hq-srv {\n\thardware ethernet $mac;\n\tfixed-address 172.16.100.2;\n}" >> /etc/dhcp/dhcpd.conf
+echo -e "subnet 172.16.100.0 netmask 255.255.255.192 {\n  range 172.16.100.2 172.16.100.62;\n  option routers 172.16.100.1;\n  default-lease-time 600;\n  max-lease-time 7200;\n}\nhost hq-srv {\n\thardware ethernet $mac;\n\tfixed-address 172.16.100.2;\n}" >> /etc/dhcp/dhcpd.conf
 echo -e "DHCPDARGS=ens19" >>/etc/sysconfig/dhcpd
 systemctl restart dhcpd
 systemctl enable --now dhcpd
