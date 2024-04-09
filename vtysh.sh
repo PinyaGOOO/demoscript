562a929edf8051bf79fb46caa1326a919aa6d462
PinyaGOOO <165675370+PinyaGOOO@users.noreply.github.com>
2024-04-09 15:43:07 +0300

    Add files via upload

--- vtysh.sh
+++ vtysh.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
-vtysh -c "configure terminal" \
-    -c "router ospf" \
-    -c "passive-interface default" \
-    -c "network 172.16.100.0/26 area 0" \
-    -c "exit" \
-    -c "interface tun1" \
-    -c "no ip ospf network broadcast" \
-    -c "no ip ospf passive" \
-    -c "exit" \
-    -c "do write"
-
-
+echo '#!/bin/bash\n\ndata=$(date +%d.%m.%Y-%H:%M:%S)\nmkdir /var/backup/$data\ncp -r /etc/frr /var/backup/$data\ncp -r /etc/nftables /var/backup/$data\ncp -r /etc/NetworkManager/system-connections /var/backup/$data\ncp -r /etc/dhcp /var/backup/$data\ncd /var/backup\ntar czfv "./$data.tar.gz" ./$data\nrm -r /var/backup/$data' > /var/backup-scripts/backup.sh
 
 
 
