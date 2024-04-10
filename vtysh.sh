92f110e2202a85eda842bc00fa48f1fab732ce72
PinyaGOOO <165675370+PinyaGOOO@users.noreply.github.com>
2024-04-10 18:57:29 +0300

    Add files via upload

--- vtysh.sh
+++ vtysh.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-echo '#!/bin/bash\n\ndata=$(date +%d.%m.%Y-%H:%M:%S)\nmkdir /var/backup/$data\ncp -r /etc/frr /var/backup/$data\ncp -r /etc/nftables /var/backup/$data\ncp -r /etc/NetworkManager/system-connections /var/backup/$data\ncp -r /etc/dhcp /var/backup/$data\ncd /var/backup\ntar czfv "./$data.tar.gz" ./$data\nrm -r /var/backup/$data' > /var/backup-scripts/backup.sh
+echo -e "default-lease-time 2592000;\npreferred-lifetime 604800;\noption dhcp-renewal-time 3600;\noption dhcp-rebinding-time 7200;\n\nallow leasequery;\n\noption dhcp6.preference 255;\noption dhcp6.info-refresh-time 21600;\n\nsubnet6 FD24:172::/122 {\n\trange6 FD24:172::2 FD24:172::12;\n}\n" > /etc/dhcp/dhcpd6.conf
+
+systemctl enable --now dhcpd6
 
 
 
